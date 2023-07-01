5dd534b07c4ac8b738d80d8f196b2f20b6563f07
stephano-tri <mtriumph23@gmail.com>
2023-07-02 01:00:42 +0900

    FEATURE: scheduler function added and testing #3

--- src/main/kotlin/eom/tri/weather/service/WeatherDataCollectingService.kt
+++ src/main/kotlin/eom/tri/weather/service/WeatherDataCollectingService.kt
@@ -1,6 +1,17 @@
 package eom.tri.weather.service
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Service
+
+@Service
 class WeatherDataCollectingService {
+    val logger: Logger= LoggerFactory.getLogger(WeatherDataCollectingService::class.java)
 
 
+    @Scheduled(fixedDelay = 1000)
+    fun collectWeatherData() {
+        logger.info("collectWeatherData func executed")
+    }
 }
